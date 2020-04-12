785e2a4ed6c8ec1e35e10211483885647fecf8ad
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-12 11:45:18 -0700

    Create MovieList.jsx, index.js, and css stylesheet

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Build out functional stateless MovieList
+// Div class="movie-list"
+// Unordered list, with li elements
+// Iterate over passed in movie data, and create a li element for each
+
+
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import React
+// List out movie data
+// Render MovieList to the DOM
+
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
