dda5cdffd8d11c8f30656b06585a6597c4283770
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-12 11:56:15 -0700

    Build out functional stateless MovieList component and create an li element for each movie in movie data

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
 // Build out functional stateless MovieList
 // Div class="movie-list"
 // Unordered list, with li elements
 // Iterate over passed in movie data, and create a li element for each
+
+var MovieList = (props) => (
+  // Fill out MovieList body with html and jsx
+  <div className="movie-list">
+    <h1>Movie List</h1>
+    <ul>
+      {props.movies.map(movie => {
+        return <li className="movie-item">{movie.title}</li>
+      })}
+    </ul>
+  </div>
+)
+
+export default MovieList;
 
 
 
