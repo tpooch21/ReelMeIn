d519a453ed50e8c62d5b2f579032a2065e0c9eee
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-14 22:13:17 -0700

    Add three  unit tests for MovieList

--- spec/components/MovieListSpec.jsx
+++ spec/components/MovieListSpec.jsx
@@ -1,5 +1,6 @@
 import MovieList from '../../src/components/MovieList.js';
 import fakeMovieData from '../data/fakeMovieData.js';
+import MovieListEntry from '../../src/components/MovieListEntry.js';
 
 describe('MovieList', function() {
 
@@ -9,12 +10,41 @@ describe('MovieList', function() {
   });
   // Should render 1 movie when given 1 movie
   it('should render 1 movie when given one movie', function() {
-    var movieList = <MovieList movies={fakeMovieData.slice(-1)} />
-    expect(movieList.props.movies).to.have.length(1);
-  })
+    const renderer = new ReactShallowRenderer();
+
+    renderer.render(
+      <MovieList movies={fakeMovieData.slice(-1)} />
+    );
+
+    const movieList = renderer.getRenderOutput();
+    expect(movieList.props.children).to.have.length(1);
+    movieList.props.children.forEach(child => expect(child.type).to.equal(MovieListEntry));
+  });
 
   // Should render 3 movies when given 3 movies
+  it ('should render 3 movies when given 3 movies', () => {
+    const renderer = new ReactShallowRenderer();
+
+    renderer.render(
+      <MovieList movies={fakeMovieData.slice(-3)} />
+    );
+
+    const movieList = renderer.getRenderOutput();
+    expect(movieList.props.children).to.have.length(3);
+    movieList.props.children.forEach(child => expect(child.type).to.equal(MovieListEntry));
+  });
 
   // Should render 5 movies when given 5 movies
+  it ('should render 5 movies when given 5 movies', () => {
+    const renderer = new ReactShallowRenderer();
+
+    renderer.render(
+      <MovieList movies={fakeMovieData.slice(-5)} />
+    );
+
+    const movieList = renderer.getRenderOutput();
+    expect(movieList.props.children).to.have.length(5);
+    movieList.props.children.forEach(child => expect(child.type).to.equal(MovieListEntry));
+  });
 
 })
