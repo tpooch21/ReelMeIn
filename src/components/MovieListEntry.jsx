db09ed033144d908a42d77e07dfc1545403ab3f2
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-19 12:51:54 -0700

    Add 'watched' buttons to each movieListEntry and map out Watched and toWatch list implementation plans with pseudocode

--- src/components/MovieListEntry.jsx
+++ src/components/MovieListEntry.jsx
@@ -2,9 +2,13 @@
 // MovieListEntries will have access to movie titles, descriptions, etc from their props
 // Functional stateless components
 
+// Add a little button to each entry 'watched' that handles click Event onUserWatch in App
+
+
 var MovieListEntry = (props) => (
   <div className="movie-list-entry">
     <h5>{props.movie.title}</h5>
+    <button className="watched-button">Watched</button>
   </div>
 );
 
